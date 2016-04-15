873935ec59a771124c2ccf2694732e8f9df926d6
heck82 <andriy.khekalo@edvantis.com>
2016-04-15 21:59:28 +0300

    add serv

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-var http = require('http');
-var static = require('node-static');
-var file = new static.Server('.');
+var express  = require('express');
+var app      = express();
+var port  	 = process.env.PORT || 8080;
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-http.createServer(function(req, res) {
-  file.serve(req, res);
-}).listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/')); 		// set the static files location /public/img will be /img for users
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
-console.log('Server running on port 8080');
+app.listen(port);
+console.log("App listening on port " + port);
